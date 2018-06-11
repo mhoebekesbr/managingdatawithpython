e6bb493f2acc53e8a902e610e10def8fec7d73f5
Mark Hoebeke <mark.hoebeke@sb-roscoff.fr>
2018-06-11 15:20:12 +0200

    Removed sing-line fasta files. Enhanced solutions to exercices. Removed ex17 (openpyxl has been removed from training programme) and renumbered subsequent exercices accordingly.

--- src/ex19/gettaxinfo.py
+++ src/ex19/gettaxinfo.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import configparser
 import json
 import requests
 
@@ -9,28 +10,51 @@ BASEURL='http://www.marinespecies.org/rest/AphiaRecordByAphiaID/'
 parser=argparse.ArgumentParser()
 parser.add_argument('-i','--infile',help='CSV file where the last column of each line contains an AphiaID')
 parser.add_argument('-o','--outfile',help='CSV file with the taxonomic information extracted from WoRMS')
+parser.add_argument('-c','--configfile',help='configuration file with input and ouput file format parameters.')
+parser.add_argument('-w','--writeconfig',help='write the default configuration for input and ouput file format parameters.')
+
 args=parser.parse_args()
 
-COLUMN_HEADERS=['AphiaID','kingdom','phylum','class','order','family','genus','scientificname','authority']
+ALL_COLUMN_HEADERS=['AphiaID','kingdom','phylum','class','order','family','genus','scientificname','authority']
 
-allTaxonInfo=[]
-with open(args.infile) as csvfile :
-    reader=csv.DictReader(csvfile)
-    for record in reader :
-        aphiaid=record['aphiaid']
-        url=BASEURL+aphiaid
-        r=requests.get(url)
-        jsonString=r.text
-        completeTaxonInfo=json.loads(jsonString)
-        summaryTaxonInfo={}
-        for header in COLUMN_HEADERS :
-            summaryTaxonInfo[header]=completeTaxonInfo[header]
-        allTaxonInfo.append(summaryTaxonInfo)
+config=configparser.ConfigParser()
 
-with open(args.outfile,'w') as csvfile :
-    writer=csv.DictWriter(csvfile,fieldnames=COLUMN_HEADERS,delimiter=";")
-    writer.writeheader()
-    writer.writerows(allTaxonInfo)
+
+
+if args.configfile is not None:
+    config.read(args.configfile)
+else:
+    config['outfile']={}
+    for header in ALL_COLUMN_HEADERS:
+        config['outfile'][header]='yes'
+
+if args.writeconfig is not None:
+    with open(args.writeconfig, 'w') as cfgfile:
+        config.write(cfgfile)
+else:
+    allTaxonInfo=[]
+    column_headers=[]
+    for header in ALL_COLUMN_HEADERS:
+        if config['outfile'].getboolean(header) is True :
+            column_headers.append(header)
+
+    with open(args.infile) as csvfile :
+        reader=csv.DictReader(csvfile)
+        for record in reader :
+            aphiaid=record['aphiaid']
+            url=BASEURL+aphiaid
+            r=requests.get(url)
+            jsonString=r.text
+            completeTaxonInfo=json.loads(jsonString)
+            summaryTaxonInfo={}
+            for header in column_headers :
+                summaryTaxonInfo[header]=completeTaxonInfo[header]
+            allTaxonInfo.append(summaryTaxonInfo)
+
+    with open(args.outfile,'w') as csvfile :
+        writer=csv.DictWriter(csvfile,fieldnames=column_headers,delimiter=";")
+        writer.writeheader()
+        writer.writerows(allTaxonInfo)
 
 
 
